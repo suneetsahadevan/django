3c2af349be76658124f7a8b96bbb809ded5e54ef
suneetsahadevan <suneetsahadevan@gmail.com>
2018-10-10 02:36:51 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path , include
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
