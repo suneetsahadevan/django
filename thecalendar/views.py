b20c9ee6c530f9d7f0d04d273c81de818589e6c1
suneetsahadevan <suneetsahadevan@gmail.com>
2018-10-22 16:49:20 +0530

    gets current calendar

--- thecalendar/views.py
+++ thecalendar/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Event
+import datetime , calendar
 
 # Create your views here.
 def calendar_detail(request):
+    now = datetime.datetime.now()
+    numberofdays = calendar.monthrange(now.year, now.month)[1]
+    monthname = calendar.month_name[now.month]
+    year = now.year
+    monthdays = []
+    for i in range(numberofdays):
+        monthdays.append(i+1)
     events = Event.objects.all()
-    return render(request, 'thecalendar/base.html', {'events': events})
+    return render(request, 'thecalendar/base.html', {'monthdays': monthdays , 'monthname': monthname ,'year' : year})
