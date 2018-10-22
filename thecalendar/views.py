aacaf8daa019c2caaf91cffdda989e920ba74137
suneetsahadevan <suneetsahadevan@gmail.com>
2018-10-23 00:17:31 +0530

    improvements

--- thecalendar/views.py
+++ thecalendar/views.py
@@ -6,10 +6,12 @@ import datetime , calendar
 def calendar_detail(request):
     now = datetime.datetime.now()
     numberofdays = calendar.monthrange(now.year, now.month)[1]
-    monthname = calendar.month_name[now.month]
-    year = now.year
+    currentmonthname = calendar.month_name[now.month]
+    currentyear = now.year
+    currentday = now.day
+    startdayofmonth = calendar.day_name[datetime.datetime(now.year,now.month,1).weekday()]
     monthdays = []
     for i in range(numberofdays):
         monthdays.append(i+1)
     events = Event.objects.all()
-    return render(request, 'thecalendar/base.html', {'monthdays': monthdays , 'monthname': monthname ,'year' : year})
+    return render(request, 'thecalendar/base.html', {'monthdays': monthdays , 'currentmonthname': currentmonthname ,'currentyear' : currentyear , 'currentday' : currentday , 'startdayofmonth': startdayofmonth})
