164718b06edb7b21146a4752cf0addf481357d8c
suneetsahadevan <suneetsahadevan@gmail.com>
2018-12-03 16:58:47 +0530

    post method

--- thecalendar/forms.py
+++ thecalendar/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
 class Get_year_month(forms.Form):
-    form_date = forms.DateField(help_text="This is a date field")
+    date = forms.DateField(
+    required=False, widget=MonthYearWidget(years=xrange(2017,2010))
+    )
--- thecalendar/views.py
+++ thecalendar/views.py
@@ -1,30 +1,34 @@
 from django.shortcuts import render
 from .models import Event
 import datetime , calendar
+from .forms import Get_year_month
 
 # Create your views here.
 def calendar_detail(request):
-    now = datetime.datetime.now()
-    numberofdays = calendar.monthrange(now.year, now.month)[1]
-    currentmonthname = calendar.month_name[now.month]
-    currentyear = now.year
-    currentday = now.day
-    startdayofmonth = calendar.day_name[datetime.datetime(now.year,now.month,1).weekday()]
-    startdaynumber = datetime.datetime(now.year,now.month,1).weekday()
-    enddaynumber = datetime.datetime(now.year,now.month,numberofdays).weekday()
-    enddays = 6 - enddaynumber
-    startblanks = []
-    endblanks = []
-    i = 0
-    for i in range(startdaynumber):
-        startblanks.append(i)
+    if request.method == "POST":
+        form = Get_year_month(request.POST)
+    else:
+        now = datetime.datetime.now()
+        numberofdays = calendar.monthrange(now.year, now.month)[1]
+        currentmonthname = calendar.month_name[now.month]
+        currentyear = now.year
+        currentday = now.day
+        startdayofmonth = calendar.day_name[datetime.datetime(now.year,now.month,1).weekday()]
+        startdaynumber = datetime.datetime(now.year,now.month,1).weekday()
+        enddaynumber = datetime.datetime(now.year,now.month,numberofdays).weekday()
+        enddays = 6 - enddaynumber
+        startblanks = []
+        endblanks = []
+        i = 0
+        for i in range(startdaynumber):
+            startblanks.append(i)
 
-    monthdays = []
-    i = 0
-    for i in range(numberofdays):
-        monthdays.append(i+1)
-    i = 1
-    for i in range(6 - enddaynumber):
-        endblanks.append(i)
-    events = Event.objects.all()
-    return render(request, 'thecalendar/base.html', {'monthdays': monthdays , 'currentmonthname': currentmonthname ,'currentyear' : currentyear , 'currentday' : currentday , 'startdayofmonth': startdayofmonth , 'startblanks' : startblanks , 'endblanks' : endblanks})
+        monthdays = []
+        i = 0
+        for i in range(numberofdays):
+            monthdays.append(i+1)
+        i = 1
+        for i in range(6 - enddaynumber):
+            endblanks.append(i)
+        events = Event.objects.all()
+        return render(request, 'thecalendar/base.html', {'monthdays': monthdays , 'currentmonthname': currentmonthname ,'currentyear' : currentyear , 'currentday' : currentday , 'startdayofmonth': startdayofmonth , 'startblanks' : startblanks , 'endblanks' : endblanks})
